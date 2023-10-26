ec6973fd94eea75375f5e575087d8021e4ff2adf
Greg Kostin <gkostin@umich.edu>
2023-10-26 16:11:26 -0400

    comment out Rack::Attack

--- app/config/app.rb
+++ app/config/app.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "hanami"
-require "rack/attack"
+# require "rack/attack"
 
 module Lauth
   # Defines global application configuration.
@@ -14,10 +14,10 @@ module Lauth
       csp[:script_src] += " 'unsafe-eval' 'unsafe-inline' https://unpkg.com/"
     end
 
-    Rack::Attack.safelist("allow localhost") { |request| %w[127.0.0.1 ::1].include? request.ip }
-    Rack::Attack.throttle("requests by IP", limit: 100, period: 60, &:ip)
+    # Rack::Attack.safelist("allow localhost") { |request| %w[127.0.0.1 ::1].include? request.ip }
+    # Rack::Attack.throttle("requests by IP", limit: 100, period: 60, &:ip)
 
-    config.middleware.use Rack::Attack
+    # config.middleware.use Rack::Attack
     config.middleware.use Rack::Deflater
     config.middleware.use Rack::Static,
                           {root: "public", urls: %w[/icon.svg /manifest.webmanifest /stylesheets]}
