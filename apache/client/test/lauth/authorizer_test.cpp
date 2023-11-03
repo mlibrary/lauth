4f830b2fa65b5ccd276312b568533b34663d6b4d
Noah Botimer <botimer@umich.edu>
2023-11-03 16:21:01 -0400

    Remove spurious authorizer test
    This was a technical spike test that we accidentally left around, now
    that there are behavioral tests in place.

--- apache/client/test/lauth/authorizer_test.cpp
+++ apache/client/test/lauth/authorizer_test.cpp
@@ -16,11 +16,6 @@ using ::testing::_;
 using mlibrary::lauth::ApiClient;
 using mlibrary::lauth::Authorizer;
 
-TEST(AuthorizerTest, inject_api_client) {
-  auto client = std::make_unique<ApiClient>();
-  Authorizer authorizer(std::move(client));
-}
-
 TEST(AuthorizerTest, AllowsAccessWhenApiSaysAuthorized) {
   auto client = std::make_unique<MockApiClient>();
   EXPECT_CALL(*client, isAllowed(_)).WillOnce(Return(true));
