9e06e3ffcb7d53e4b6410eb1c613ef4a39bb5183
Noah Botimer <botimer@umich.edu>
2023-11-08 14:27:18 -0500

    Build out mock API niceties for integration testing
    - Set up separate exe/test in meson build
    - Move mock_service out of "lauth" directory
    - Add a stop endpoint to deal with server missing ctrl-c/SIGINT
    - Give some output at the root URL
    - Print to stdout when requests come in
    - Auto-select port or take command line parameter
    - Add mock-api service to Compose file
    
    Run on the default port (9000) for testing like so:
    
    docker compose up mock-api --build

---
+++ apache/client/test/mock_service.cpp
@@ -0,0 +1,38 @@
+#include <httplib.h>
+
+int main(int argc, char **argv) {
+  using namespace httplib;
+
+  int port;
+  const std::string address = "0.0.0.0";
+  Server server;
+
+  if (argc == 2) {
+    port = std::atoi(argv[1]);
+    server.bind_to_port(address, port);
+  } else {
+    port = server.bind_to_any_port(address);
+  }
+
+  server.Get("/", [](const Request &req, Response &res) {
+    res.set_content("Root", "text/plain");
+  });
+
+  server.Get("/stop", [&](const Request &req, Response &res) {
+    res.set_content("Shutting down server...", "text/plain");
+    server.stop();
+  });
+
+  server.Get("/users/:id/is_allowed", [](const Request &req, Response &res) {
+    auto user_id = req.path_params.at("id");
+    std::cout << "Got request for /users/:id/is_allowed" << std::endl;
+    res.set_content("no", "text/plain");
+  });
+
+  std::cout << "Listening on http://" << address << ":" << port << std::endl;
+  std::cout << "Stop URL: http://" << address << ":" << port << "/stop"
+            << std::endl;
+  server.listen_after_bind();
+  std::cout << "Server has stopped... Bye!" << std::endl;
+  return 0;
+}
