3972d004cae8e19f49c42d40b49d091d3315c584
Greg Kostin <gkostin@umich.edu>
2023-11-09 09:49:12 -0500

    quick fix to client test

--- apache/client/src/lauth/http_client.cpp
+++ apache/client/src/lauth/http_client.cpp
@@ -3,6 +3,10 @@
 #include <httplib.h>
 
 namespace mlibrary::lauth {
+  bool HttpClient::isAllowed(Request req) {
+    return req.user == "authorized";
+  }
+
   std::string HttpClient::get(const std::string& path) {
     httplib::Client client(baseUrl);
 
