ab088ceefab1a92e6fb7cecc107c0f80be071463
Bryan Hockey <bhock@umich.edu>
2023-12-21 14:58:59 -0500

    rubocop

--- lauth/spec/support/factories/network.rb
+++ lauth/spec/support/factories/network.rb
@@ -3,8 +3,11 @@ require "ipaddr"
 Factory.define(:network, struct_namespace: Lauth) do |f|
   f.sequence(:uniqueIdentifier) { |n| n }
   f.dlpsCIDRAddress { "0.0.0.0/0" }
+  # standard:disable Naming/VariableName, Naming/BlockParameterName
   f.dlpsAddressStart { |dlpsCIDRAddress| IPAddr.new(dlpsCIDRAddress).to_range.first.to_i }
   f.dlpsAddressEnd { |dlpsCIDRAddress| IPAddr.new(dlpsCIDRAddress).to_range.last.to_i }
+  # standard:enable Naming/VariableName, Naming/BlockParameterName
+
   f.dlpsAccessSwitch "allow"
   f.lastModifiedTime Time.now
   f.lastModifiedBy "root"
