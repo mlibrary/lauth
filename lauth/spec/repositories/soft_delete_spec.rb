653193aeb12a0b858ead6417a8f8f1375a2635ae
Greg Kostin <gkostin@umich.edu>
2023-12-22 14:37:40 -0500

    well, what do you think

--- lauth/spec/repositories/soft_delete_spec.rb
+++ lauth/spec/repositories/soft_delete_spec.rb
@@ -1,128 +1,127 @@
 # require "hanami_helper"
 
 RSpec.describe "Soft Delete", type: :database do
-  subject(:repo) { Lauth::Repositories::GrantRepo.new }
+  def contains_exactly_grants?(grants1, grants2)
+    return false if grants1.count != grants2.count
 
-  it "location" do
-    collection = Factory[:collection, :restricted_by_username]
-    Hanami.app["persistence.rom"]["relations"].locations.by_pk(collection.locations.first.dlpsServer).changeset(:update, dlpsDeleted: "t").commit
-    collection = Hanami.app["persistence.rom"]["relations"].collections.combine(:locations).by_pk(collection.uniqueIdentifier).one
-    user = Factory[:user, userid: "lauth-allowed"]
-    _grant = Factory[:grant, :for_user, user: user, collection: collection]
-    grants = repo.for(username: "lauth-allowed", uri: "/restricted-by-username/")
-    expect(grants).to eq []
+    grants2.each do |grant|
+      return false unless grants1.map(&:uniqueIdentifier).include?(grant.uniqueIdentifier)
+    end
+
+    true
   end
 
-  context "when authorizing locations within a collection using identity-only authentication" do
-    context "with an authorized individual" do
-      subject(:grants) { repo.for(username: "lauth-allowed", uri: "/restricted-by-username/") }
+  describe "when authorizing locations within a collection using identity-only authentication" do
+    subject(:grants) { @grant_repo.for(username: "lauth-allowed", uri: "/restricted-by-username/") }
 
-      let!(:collection) { Factory[:collection, :restricted_by_username] }
-      let!(:user) { Factory[:user, userid: "lauth-allowed"] }
-      let!(:grant) { Factory[:grant, :for_user, user: user, collection: collection] }
+    before do
+      @relations = Hanami.app["persistence.rom"]["relations"]
+      @grant_repo = Lauth::Repositories::GrantRepo.new
+      @collection = Factory[:collection, :restricted_by_username]
+      @location = @collection.locations.first
+      @user = Factory[:user, userid: "lauth-allowed"]
+      @user_grant = Factory[:grant, :for_user, user: @user, collection: @collection]
+      @institution = Factory[:institution]
+      @institution_user = Factory[:user, userid: "lauth-inst-member"]
+      @institution_membership = Factory[:institution_membership, user: @institution_user, institution: @institution]
+      @institution_grant = Factory[:grant, :for_institution, institution: @institution, collection: @collection]
+      Factory[:group]
+      @group = @relations.groups.last
+      @group_user = Factory[:user, userid: "lauth-group-member"]
+      @group_membership = Factory[:group_membership, user: @group_user, group: @group]
+      @group_grant = Factory[:grant, :for_group, group: @group, collection: @collection]
+    end
 
-      context "when collection soft deleted" do
-        let!(:collection) { Factory[:collection, :restricted_by_username, dlpsDeleted: "t"] }
+    it "contains three grants" do
+      pp grants.to_a
+      expect(contains_exactly_grants?(grants, [@user_grant, @group_grant, @institution_grant])).to be true
+    end
 
-        it { expect(grants).to eq [] }
-      end
+    context "when username grant is deleted" do
+      it "contains two grants" do
+        @relations.grants.where(uniqueIdentifier: @user_grant.uniqueIdentifier).changeset(:update, dlpsDeleted: "t").commit
 
-      context "when user soft deleted" do
-        let!(:user) { Factory[:user, userid: "lauth-allowed", dlpsDeleted: "t"] }
-
-        it { expect(grants).to eq [] }
-      end
-
-      context "when grant soft deleted" do
-        let!(:grant) { Factory[:grant, :for_user, user: user, collection: collection, dlpsDeleted: "t"] }
-
-        it { expect(grants).to eq [] }
+        expect(contains_exactly_grants?(grants, [@group_grant, @institution_grant])).to be true
       end
     end
 
-    context "with a member of an authorized institution" do
-      subject(:grants) { repo.for(username: "lauth-inst-member", uri: "/restricted-by-username/") }
+    context "when collection is deleted" do
+      it "contains no grants" do
+        @relations.collections.where(uniqueIdentifier: @collection.uniqueIdentifier).changeset(:update, dlpsDeleted: "t").commit
 
-      let!(:collection) { Factory[:collection, :restricted_by_username] }
-      let!(:institution) { Factory[:institution] }
-      let!(:user) { Factory[:user, userid: "lauth-inst-member"] }
-      let!(:membership) { Factory[:institution_membership, user: user, institution: institution] }
-      let!(:grant) { Factory[:grant, :for_institution, institution: institution, collection: collection] }
-
-      context "when collection soft deleted" do
-        let!(:collection) { Factory[:collection, :restricted_by_username, dlpsDeleted: "t"] }
-
-        it { expect(grants).to eq [] }
-      end
-
-      context "when institution soft deleted" do
-        let!(:institution) { Factory[:institution, dlpsDeleted: "t"] }
-
-        it { expect(grants).to eq [] }
-      end
-
-      context "when user soft deleted" do
-        let!(:user) { Factory[:user, userid: "lauth-inst-member", dlpsDeleted: "t"] }
-
-        it { expect(grants).to eq [] }
-      end
-
-      context "when membership soft deleted" do
-        let!(:membership) { Factory[:institution_membership, user: user, institution: institution, dlpsDeleted: "t"] }
-
-        it { expect(grants).to eq [] }
-      end
-
-      context "when grant soft deleted" do
-        let!(:grant) { Factory[:grant, :for_institution, institution: institution, collection: collection, dlpsDeleted: "t"] }
-
-        it { expect(grants).to eq [] }
+        expect(grants.first).to eq nil
       end
     end
 
-    context "with a member of an authorized group" do
-      subject(:grants) { repo.for(username: "lauth-group-member", uri: "/restricted-by-username/") }
+    context "when location is deleted" do
+      it "contains no grants" do
+        @relations.locations.where(dlpsPath: "/restricted-by-username%").changeset(:update, dlpsDeleted: "t").commit
 
-      let!(:collection) { Factory[:collection, :restricted_by_username] }
-      let!(:user) { Factory[:user, userid: "lauth-group-member"] }
-      let!(:group) {
-        Factory[:group]
-        relations.groups.last
-      }
-      let!(:membership) { Factory[:group_membership, user: user, group: group] }
-      let!(:grant) { Factory[:grant, :for_group, group: group, collection: collection] }
-
-      context "when collection soft deleted" do
-        let!(:collection) { Factory[:collection, :restricted_by_username, dlpsDeleted: "t"] }
-
-        it { expect(grants).to eq [] }
+        expect(grants.first).to eq nil
       end
+    end
 
-      context "when user soft deleted" do
-        let!(:user) { Factory[:user, userid: "lauth-group-member", dlpsDeleted: "t"] }
+    context "when username is deleted" do
+      it "contains no grants" do
+        @relations.users.where(userid: @user.userid).changeset(:update, dlpsDeleted: "t").commit
 
-        it { expect(grants).to eq [] }
+        expect(grants.first).to eq nil
       end
+    end
 
-      context "when group soft deleted" do
-        let!(:group) {
-          Factory[:group, dlpsDeleted: "t"]
-          relations.groups.last
-        }
+    context "when institution is deleted" do
+      it "contains two grants" do
+        @relations.institutions.where(uniqueIdentifier: @institution.uniqueIdentifier).changeset(:update, dlpsDeleted: "t").commit
 
-        it { expect(grants).to eq [] }
+        expect(contains_exactly_grants?(grants, [@group_grant, @user_grant])).to be true
       end
+    end
 
-      context "when membership soft deleted" do
-        let!(:membership) { Factory[:group_membership, group: group, user: user, dlpsDeleted: "t"] }
+    context "when institution membership is deleted" do
+      it "contains two grants" do
+        @relations.institution_memberships.where(userid: @institution_user.userid).changeset(:update, dlpsDeleted: "t").commit
 
-        it { expect(grants).to eq [] }
+        expect(contains_exactly_grants?(grants, [@group_grant, @user_grant])).to be true
       end
+    end
 
-      context "when grant soft deleted" do
-        let!(:grant) { Factory[:grant, :for_group, group: group, collection: collection, dlpsDeleted: "t"] }
+    context "when institution user is deleted" do
+      it "contains three grants" do
+        @relations.users.where(userid: @institution_user.userid).changeset(:update, dlpsDeleted: "t").commit
 
-        it { expect(grants).to eq [] }
+        pp @relations.users.to_a
+        pp @relations.grants.to_a
+        pp grants.to_a
+
+        expect(contains_exactly_grants?(grants, [@institution_grant, @group_grant, @user_grant])).to be true
+      end
+    end
+
+    context "when group is deleted" do
+      it "contains two grants" do
+        @relations.groups.where(uniqueIdentifier: @group.uniqueIdentifier).changeset(:update, dlpsDeleted: "t").commit
+
+        expect(contains_exactly_grants?(grants, [@user_grant, @institution_grant])).to be true
+      end
+    end
+
+    context "when group membership is deleted" do
+      it "contains two grants" do
+        @relations.group_memberships.where(userid: @group_user.userid).changeset(:update, dlpsDeleted: "t").commit
+
+        expect(contains_exactly_grants?(grants, [@user_grant, @institution_grant])).to be true
+      end
+    end
+
+    context "when group user is deleted" do
+      it "contains three grants" do
+        @relations.users.where(userid: @group_user.userid).changeset(:update, dlpsDeleted: "t").commit
+
+        pp @relations.users.to_a
+        pp @relations.grants.to_a
+        pp grants.to_a
+
+        expect(contains_exactly_grants?(grants, [@user_grant, @group_grant, @institution_grant])).to be true
       end
     end
   end
