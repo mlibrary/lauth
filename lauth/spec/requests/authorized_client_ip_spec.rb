d646ab4d19da154dbc13633a7fedb7d189892e24
Bryan Hockey <bhock@umich.edu>
2024-01-04 12:24:54 -0500

    Reword test names

--- lauth/spec/requests/authorized_client_ip_spec.rb
+++ lauth/spec/requests/authorized_client_ip_spec.rb
@@ -1,4 +1,75 @@
 RSpec.describe "/authorized by client-ip", type: [:request, :database] do
+
+  let!(:institution) { Factory[:institution] }
+  let!(:collection) { Factory[:collection, :restricted_by_client_ip] }
+  let!(:grant) { Factory[:grant, :for_institution, institution: institution, collection: collection] }
+
+  context "(allow>none) given an allowed network only" do
+    let!(:network) { create_network("allow", "10.1.16.0/24") }
+
+    it "allows an unknown user within the network" do
+      expect(request_from("10.1.16.2")).to eq({determination: "allowed"})
+    end
+
+    it "denies an unknown user outside the network" do
+      expect(request_from("10.1.17.1")).to eq({determination: "denied"})
+    end
+
+  end
+
+  context "(allow>allow) given an allowed enclave within an allowed network" do
+    let!(:network) { create_network("allow", "10.1.6.0/24") }
+    let!(:enclave) { create_network("allow", "10.1.6.8/29") }
+    it "allows an unknown user within the enclave" do
+      expect(request_from("10.1.6.9")).to eq({determination: "allowed"})
+    end
+    it "is allowed outside the enclave" do
+      expect(request_from("10.1.6.7")).to eq({determination: "allowed"})
+    end
+  end
+
+  context "(allow>deny) given a denied enclave within an allowed network" do
+    let!(:network) { create_network("allow", "10.1.6.0/24") }
+    let!(:enclave) { create_network("deny", "10.1.6.2/32") }
+    it "denies an unknown user within the enclave" do
+      expect(request_from("10.1.6.2")).to eq({determination: "denied"})
+    end
+    it "allows an unknown user outside the enclave" do
+      expect(request_from("10.1.6.44")).to eq({determination: "allowed"})
+    end
+  end
+
+  context "(deny>allow) given an allowed enclave within a denied network" do
+    let!(:network) { create_network("deny", "10.1.7.0/24") }
+    let!(:enclave) { create_network("allow", "10.1.7.8/29") }
+    it "allows an unknown user within the enclave" do
+      expect(request_from("10.1.7.14")).to eq({determination: "allowed"})
+    end
+    it "denies an unknown user outside the enclave" do
+      expect(request_from("10.1.7.63")).to eq({determination: "denied"})
+    end
+  end
+
+  context "(deny>deny) given a denied enclave within a denied network" do
+    let!(:network) { create_network("deny", "10.1.17.0/24") }
+    let!(:enclave) { create_network("deny", "10.1.17.8/29") }
+    it "denies an unknown user within the enclave" do
+      expect(request_from("10.1.17.12")).to eq({determination: "denied"})
+    end
+    it "denies an unknown user outside the enclave" do
+      expect(request_from("10.1.17.63")).to eq({determination: "denied"})
+    end
+  end
+
+  context "(deny>none) given a denied network only" do
+    let!(:network) { create_network("deny", "10.1.17.0/24") }
+    it "denies an unknown user within the network" do
+      expect(request_from("10.1.17.2")).to eq({determination: "denied"})
+    end
+  end
+
+  private
+
   # Convenience for building networks.
   # Requires 'institution' be set in a let block.
   # @param access [String] Either 'allow' or 'deny'
@@ -19,70 +90,4 @@ RSpec.describe "/authorized by client-ip", type: [:request, :database] do
     JSON.parse(last_response.body, symbolize_names: true)
   end
 
-  let!(:institution) { Factory[:institution] }
-  let!(:collection) { Factory[:collection, :restricted_by_client_ip] }
-  let!(:grant) { Factory[:grant, :for_institution, institution: institution, collection: collection] }
-
-  context "(allow>none) given an allowed network only" do
-    let!(:network) { create_network("allow", "10.1.16.0/24") }
-
-    it "is allowed within the network" do
-      expect(request_from("10.1.16.2")).to eq({determination: "allowed"})
-    end
-
-    it "is denied outside the network" do
-      expect(request_from("10.1.17.1")).to eq({determination: "denied"})
-    end
-  end
-
-  context "(allow>allow) given an allowed enclave within an allowed network" do
-    let!(:network) { create_network("allow", "10.1.6.0/24") }
-    let!(:enclave) { create_network("allow", "10.1.6.8/29") }
-    it "is allowed within the enclave" do
-      expect(request_from("10.1.6.9")).to eq({determination: "allowed"})
-    end
-    it "is allowed outside the enclave" do
-      expect(request_from("10.1.6.7")).to eq({determination: "allowed"})
-    end
-  end
-
-  context "(allow>deny) given a denied enclave within an allowed network" do
-    let!(:network) { create_network("allow", "10.1.6.0/24") }
-    let!(:enclave) { create_network("deny", "10.1.6.2/32") }
-    it "is denied within the enclave" do
-      expect(request_from("10.1.6.2")).to eq({determination: "denied"})
-    end
-    it "is allowed outside the enclave" do
-      expect(request_from("10.1.6.44")).to eq({determination: "allowed"})
-    end
-  end
-
-  context "(deny>allow) given an allowed enclave within a denied network" do
-    let!(:network) { create_network("deny", "10.1.7.0/24") }
-    let!(:enclave) { create_network("allow", "10.1.7.8/29") }
-    it "is allowed within the enclave" do
-      expect(request_from("10.1.7.14")).to eq({determination: "allowed"})
-    end
-    it "is denied outside the enclave" do
-      expect(request_from("10.1.7.63")).to eq({determination: "denied"})
-    end
-  end
-
-  context "(deny>deny) given a denied enclave within a denied network" do
-    let!(:network) { create_network("deny", "10.1.17.0/24") }
-    let!(:enclave) { create_network("deny", "10.1.17.8/29") }
-    it "is denied within the enclave" do
-      expect(request_from("10.1.17.12")).to eq({determination: "denied"})
-    end
-    it "is denied outside the enclave" do
-      expect(request_from("10.1.17.63")).to eq({determination: "denied"})
-    end
-  end
-
-  context "(deny>none) given a denied network only" do
-    let!(:network) { create_network("deny", "10.1.17.0/24") }
-    it "is denied within the network" do
-      expect(request_from("10.1.17.2")).to eq({determination: "denied"})
-    end
-  end
 end
