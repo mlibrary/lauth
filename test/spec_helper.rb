dd1349fe6f1d9fe6be2d9f6e787766fefc85988f
Bryan Hockey <bhock@umich.edu>
2024-01-09 15:10:30 -0500

    Add AuthUsers convenience methods

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -9,17 +9,33 @@ class HttpCodes
 end
 # standard:enable Naming/ConstantName
 
+module AuthUsers
+  def bad_user
+    "lauth-denied"
+  end
+
+  def good_user
+    "lauth-allowed"
+  end
+
+  def inst_user
+    "lauth-inst-member"
+  end
+end
+
 module BasicAuth
+  include AuthUsers
+
   def basic_auth_bad_user
-    "Basic #{Base64.urlsafe_encode64("lauth-denied:lauth-denied")}"
+    "Basic #{Base64.urlsafe_encode64("#{bad_user}:lauth-denied")}"
   end
 
   def basic_auth_good_user
-    "Basic #{Base64.urlsafe_encode64("lauth-allowed:lauth-allowed")}"
+    "Basic #{Base64.urlsafe_encode64("#{good_user}:lauth-allowed")}"
   end
 
   def basic_auth_inst_member
-    "Basic #{Base64.urlsafe_encode64("lauth-inst-member:lauth-inst-member")}"
+    "Basic #{Base64.urlsafe_encode64("#{inst_user}:lauth-inst-member")}"
   end
 end
 
