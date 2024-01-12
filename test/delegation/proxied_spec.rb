36d7b6d8352ea6935effaacd4e397c84ec891195
Bryan Hockey <bhock@umich.edu>
2024-01-12 16:08:34 -0500

    Add test/delegation/hosted_spec

--- test/delegation/proxied_spec.rb
+++ test/delegation/proxied_spec.rb
@@ -1,5 +1,58 @@
-RSpec.describe "A proxed application in delegated mode" do
+RSpec.describe "A proxied application in delegated mode" do
   # These are apps external to the web server, configured for reverse proxy.
   # They should receive identity and the authorized collections in forwarded
   # headers.
+
+  include AuthUsers
+  context "when logged in as an authorized user" do
+    subject(:response) do
+      website.get("/app/hosted") do |req|
+        req.headers["X-Forwarded-User"] = good_user
+      end
+    end
+    it "is OK" do
+      expect(response.status).to eq HttpCodes::OK
+    end
+
+    it "lists the matching public collections" do
+      expect(parse(response.body)["X-Public-Coll"]&.split(":"))
+        .to contain_exactly 'public-cats'
+    end
+
+    it "lists the matching authorized collections" do
+      expect(parse(response.body)["X-Authzd-Coll"]&.split(":"))
+        .to contain_exactly 'extra-cats', 'foia-cats'
+    end
+  end
+
+  context "when not logged in" do
+    subject(:response) { website.get("/app/hosted") }
+    it "is OK" do
+      expect(response.status).to eq HttpCodes::OK
+    end
+
+    it "lists the matching public collections" do
+      expect(parse(response.body)["X-Public-Coll"]&.split(":"))
+        .to contain_exactly 'domestic-cats', 'foia-cats'
+    end
+
+    it "lists the matching authorized collections" do
+      expect(parse(response.body)["X-Authzd-Coll"]&.split(":"))
+        .to be_empty
+    end
+  end
+
+  private
+
+  def parse(response_body)
+    response_body
+      .split("\n")
+      .map{|s| s.split(":", 2)}
+      .to_h
+  end
+
+  def website
+    @website ||= Faraday.new(TestSite::URL)
+  end
+
 end
