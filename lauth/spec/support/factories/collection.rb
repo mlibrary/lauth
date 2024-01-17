3f6ae25bcb390eddc8ca4fefc1970ad9c008ca20
Bryan Hockey <bhock@umich.edu>
2024-01-17 14:10:11 -0500

    Factories for proxied, hosted collection, location

--- lauth/spec/support/factories/collection.rb
+++ lauth/spec/support/factories/collection.rb
@@ -3,6 +3,7 @@ Factory.define(:collection, struct_namespace: Lauth) do |f|
   f.sequence(:commonName) { |n| "Name#{n}" }
   f.association(:locations, count: 0)
   f.manager 0
+  f.dlpsAuthzType 'n'
   f.lastModifiedTime Time.now
   f.lastModifiedBy "root"
   f.dlpsDeleted "f"
@@ -23,5 +24,17 @@ Factory.define(:collection, struct_namespace: Lauth) do |f|
     t.uniqueIdentifier "lauth-by-username-or-client-ip"
     t.dlpsAuthenMethod "any"
     t.association(:locations, :restricted_by_username_or_client_ip, count: 1)
+  end
+
+  f.trait(:delegated_to_hosted_app) do |t|
+    t.uniqueIdentifier "delegated-to-hosted-app"
+    f.dlpsAuthzType 'd'
+    t.association(:locations, :delegated_to_hosted_app, count: 1)
+  end
+
+  f.trait(:delegated_to_proxied_app) do |t|
+    t.uniqueIdentifier "delegated-to-proxied-app"
+    f.dlpsAuthzType 'd'
+    t.association(:locations, :delegated_to_proxied_app, count: 1)
   end
 end
--- lauth/spec/support/factories/location.rb
+++ lauth/spec/support/factories/location.rb
@@ -17,4 +17,13 @@ Factory.define(:location, struct_namespace: Lauth) do |f|
   f.trait(:restricted_by_username_or_client_ip) do |t|
     t.dlpsPath "/restricted-by-username-or-client-ip%"
   end
+
+  f.trait(:delegated_to_hosted_app) do |t|
+    t.dlpsPath "/delegated-to-hosted-app%"
+  end
+
+  f.trait(:delegated_to_proxied_app) do |t|
+    t.dlpsPath "/delegated-to-proxied-app%"
+  end
+
 end
