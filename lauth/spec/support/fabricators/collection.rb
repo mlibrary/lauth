af1065894b694703067cf21249f0aa295b7db3b5
Bryan Hockey <bhock@umich.edu>
2024-01-19 12:29:02 -0500

    Add collection fabricator
    We were seeing issues with the rom-factory factories where they would
    not handle associations correctly, leading to invalid results.

---
+++ lauth/spec/support/fabricators/collection.rb
@@ -0,0 +1,32 @@
+module Lauth
+  module Fab
+    class Collection< ROM::Repository[:collections]
+      include Deps[container: "persistence.rom"]
+      struct_namespace Lauth
+      commands :create
+
+      class << self
+        def create(**args)
+          new.create(default.merge(args))
+        end
+
+        def default
+          name = Faker::Creature::Animal.unique.name
+          {
+            uniqueIdentifier: "id_#{name}",
+            commonName: name,
+            manager: 0,
+            dlpsAuthenMethod: "any",
+            dlpsAuthzType: "n",
+            lastModifiedTime: Time.now,
+            lastModifiedBy: "root",
+            dlpsDeleted: "f",
+            locations: []
+          }
+        end
+
+      end
+    end
+  end
+end
+
