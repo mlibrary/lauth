35ca90a266d842ee2af96acb3a3ded8237ea4296
Bryan Hockey <bhock@umich.edu>
2024-01-24 17:25:50 -0500

    Add collections to AuthorizationResult

--- apache/client/src/lauth/authorization_result.cpp
+++ apache/client/src/lauth/authorization_result.cpp
@@ -10,11 +10,15 @@ using json = nlohmann::json;
 namespace mlibrary::lauth {
     void to_json(json& j, const AuthorizationResult& authz) {
         j = json {
-          { "determination", authz.determination}
+          { "determination", authz.determination},
+          { "public_collections", authz.public_collections},
+          { "authorized_collections", authz.authorized_collections}
         };
     }
 
     void from_json(const json& j, AuthorizationResult& authz) {
         j.at("determination").get_to(authz.determination);
+        j.at("public_collections").get_to(authz.public_collections);
+        j.at("authorized_collections").get_to(authz.authorized_collections);
     }
 }
--- apache/client/test/lauth/authorization_result_test.cpp
+++ apache/client/test/lauth/authorization_result_test.cpp
@@ -1,6 +1,7 @@
 #include "lauth/authorization_result.hpp"
 
 #include <string>
+#include <vector>
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
@@ -11,17 +12,26 @@
 using namespace mlibrary::lauth;
 
 TEST(AuthorizationResultTest, FromJson) {
-  std::string stringBody = R"({"determination":"allowed"})";
+  std::string stringBody =
+    R"({"determination":"allowed",)"
+    R"("public_collections":["pub1","pub2"],)"
+    R"("authorized_collections":["auth1","auth2"]})";
   json jsonBody = json::parse(stringBody);
   AuthorizationResult result = jsonBody.template get<AuthorizationResult>();
 
   EXPECT_THAT(result.determination, "allowed");
+  EXPECT_THAT(result.public_collections, testing::ElementsAre("pub1", "pub2"));
+  EXPECT_THAT(result.authorized_collections, testing::ElementsAre("auth1", "auth2"));
 }
 
 
 TEST(AuthorizationResultTest, ToJson) {
+  std::vector<std::string> public_collections = {"pub1", "pub2"};
+  std::vector<std::string> authorized_collections = {"auth1", "auth1"};
   AuthorizationResult result {
-    .determination = "allowed"
+    .determination = "allowed",
+    .public_collections = public_collections,
+    .authorized_collections = authorized_collections
   };
   json j = result; // magic
 
