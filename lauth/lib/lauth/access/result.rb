75d0e47b4265fc075963849e2b78bdab83343d32
Bryan Hockey <bhock@umich.edu>
2024-01-26 11:05:37 -0500

    Add defaults to Lauth::Access:Result

--- lauth/lib/lauth/access/result.rb
+++ lauth/lib/lauth/access/result.rb
@@ -1,9 +1,32 @@
 module Lauth
   module Access
-    Result = Struct.new(
-      :determination,
-      :authorized_collections,
-      :public_collections
-    )
+    class Result
+      attr_reader :determination, :authorized_collections, :public_collections
+
+      def initialize(determination: "denied", authorized_collections: [], public_collections: [])
+        @determination = determination
+        @authorized_collections = authorized_collections
+        @public_collections = public_collections
+      end
+
+      def to_h
+        {
+          determination: determination,
+          authorized_collections: authorized_collections,
+          public_collections: public_collections,
+        }
+      end
+
+      def ==(other)
+        to_h == other.to_h
+      end
+      alias eql? ==
+
+      def to_s
+        "#{self.class}<#{to_h}>"
+      end
+      alias inspect to_s
+
+    end
   end
 end
