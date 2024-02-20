4c214eefd1ff5fce325f23c44d9b80b94bd66a82
Bryan Hockey <bhock@umich.edu>
2024-02-20 15:00:03 -0500

    CollectionRepo#find_by_uri finds most specific collection

--- lauth/app/repositories/collection_repo.rb
+++ lauth/app/repositories/collection_repo.rb
@@ -4,12 +4,30 @@ module Lauth
       include Deps[container: "persistence.rom"]
       struct_namespace Lauth
 
+      # Find a collection via its uri location, specifically the dlpsPath.
+      # This prefers the "most specific" location. We define that here as
+      # the path with the deepest nesting, and in case of a tie we then prefer
+      # whichever path is longest. I.e. the path /foo/bar/baz% is more specific
+      # than /foo/bar/b%.
+      # There is an assumption that all dlpsPath values end in the SQL wildcard %.
+      # @param uri [String]
+      # @return [Collection]
       def find_by_uri(uri)
         dataset = collections
           .dataset
           .where(collections[:dlpsDeleted].is("f"))
           .join(locations.name.dataset, coll: :uniqueIdentifier, dlpsDeleted: "f")
           .where(Sequel.ilike(uri, locations[:dlpsPath]))
+          .select_append(Sequel.as( # count the slashes
+            Sequel.expr {
+              char_length(:dlpsPath) - char_length(replace(:dlpsPath, '/', ''))
+            },
+            :path_depth
+          ))
+          .order(
+            Sequel.desc(:path_depth),
+            Sequel.desc(Sequel.expr { length(:dlpsPath) } )
+          )
         collections.class.new(dataset).to_a.first
       end
 
--- lauth/spec/repositories/collection_repo_spec.rb
+++ lauth/spec/repositories/collection_repo_spec.rb
@@ -2,13 +2,16 @@ RSpec.describe Lauth::Repositories::CollectionRepo, type: :database do
   subject(:repo) { Lauth::Repositories::CollectionRepo.new }
 
   describe "#find_by_uri" do
-    it "finds the collection for the given location path" do
-      collection = Lauth::Fab::Collection.create
-      Factory[:location, dlpsPath: "/cool/path%", collection: collection]
+    it "finds the most specific collection for the given location path" do
+      expected_collection = Lauth::Fab::Collection.create(uniqueIdentifier: "expected-collection")
+      wrong_collection = Lauth::Fab::Collection.create
+      Factory[:location, dlpsPath: "/cool/p%", collection: wrong_collection]
+      Factory[:location, dlpsPath: "/uncool%", collection: wrong_collection]
+      Factory[:location, dlpsPath: "/cool/path%", collection: expected_collection]
 
       found = repo.find_by_uri("/cool/path")
 
-      expect(found.uniqueIdentifier).to eq collection.uniqueIdentifier
+      expect(found.uniqueIdentifier).to eq "expected-collection"
     end
   end
 
