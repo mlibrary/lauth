ed36215ca5ea1990b2288dcdbf5a69cc885ae7f6
Bryan Hockey <bhock@umich.edu>
2024-02-20 14:53:23 -0500

    Remove system test scenarios for projection onto proxies
    These would be identical to the other tests, and not provide additional
    coverage or value.

--- test/projection/public_in_restricted_proxy_spec.rb
+++
@@ -1,8 +0,0 @@
-RSpec.describe "Projecting public access in restricted, proxied space" do
-  # These are resources that are rewritten internally and sent back through the
-  # rewrite rules to be authorized as another collection. Specifically, the
-  # request URL would normally be in restricted space and proxied to an
-  # external app, but adopts the rules for a public collection.
-  #
-  # See public_in_restricted_spec.rb but without the proxy setup
-end
